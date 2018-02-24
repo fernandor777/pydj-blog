f4f8512b7fb6f70e364a0e2e1dfdf8861a266b2e
Fernando Miño <fernandomino7@gmail.com>
2018-02-24 02:01:18 -0500

    rest

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^post/all/$', views.post_list),
 ]
 
-
+urlpatterns = format_suffix_patterns(urlpatterns)
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Post
+from blog.serializers import PostSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Test1 blog")
+
+@api_view(['GET', 'POST'])
+def post_list(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        srl = PostSerializer(posts, many=True)
+        return Response(srl.data)
+
+
